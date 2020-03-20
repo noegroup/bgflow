e44bf8d6270fa07100624a44919343e41f6d87d8
Jonas Köhler <4534141+jonas-koehler@users.noreply.github.com>
2020-03-20 13:28:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="bgtorch",
     version="0.1",
     description="Boltzmann Generators in PyTorch",
-    author="Jonas, Koehler, Frank Noe",
+    author="Jonas, Koehler, Frank Noe, Leon Klein, Andreas Krämer",
     author_email="jonas.koehler@fu-berlin.de",
     url="https://www.mi.fu-berlin.de/en/math/groups/comp-mol-bio/index.html",
     packages=["bgtorch"],
