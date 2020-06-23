d17a6c5962016f200139e564ce69c1d2a38ceb53
Andreas Krämer <kraemer.research@gmail.com>
2020-06-23 18:25:27 +0200

    tests work; before deleting orthogonal PPPP

--- bgtorch/nn/flow/pppp.py
+++ bgtorch/nn/flow/pppp.py
@@ -5,8 +5,9 @@ Property-preserving parameter perturbations
 
 import torch
 from bgtorch.nn.flow.base import Flow
+from collections import defaultdict
 
-__all__ = ["OrthogonalPPPP", "InvertiblePPPP"]
+__all__ = ["OrthogonalPPPP", "InvertiblePPPP", "PPPPScheduler"]
 
 
 class OrthogonalPPPP(Flow):
@@ -60,7 +61,7 @@ class OrthogonalPPPP(Flow):
         xperm[..., secondhalf] = second
         return xperm[..., permutation]
 
-    def pppp_merge(self):
+    def pppp_merge(self, force_merge=True):
         """PPPP update to hidden parameters."""
         with torch.no_grad():
             #for v in [self.v, self.v + self.dv]:
@@ -144,51 +145,129 @@ class InvertiblePPPP(Flow):
         Whether to use a shift parameter (+b). If False, b=0.
     penalty_parameter : float
         Scaling factor for the regularity constraint.
+    min_logdet : float
+        The minimum for log |det W|.
+    max_logdet : float
+        The maximum for log |det W|.
+    init : str
+        Initialization. One of the following
+        - "eye": identity matrix,
+        - "reverse": reverse order
     """
-    def __init__(self, dim, shift=True, penalty_parameter=0.1, min_logdet=-2.0, max_logdet=3.0):
+    def __init__(self, dim, shift=True, penalty_parameter=0.1, min_logdet=-2, max_logdet=15, init="eye"):
         super(InvertiblePPPP, self).__init__()
         self.dim = dim
         self.u = torch.nn.Parameter(torch.zeros(dim))
         self.v = torch.nn.Parameter(torch.randn(dim))
-        self.register_buffer("A", torch.eye(dim))
-        self.register_buffer("Ainv", torch.eye(dim))
-        self.register_buffer("logdetA", torch.tensor(0.0))
+        initial_weight_matrix, initial_inverse, initial_det = {
+            "eye": (
+                torch.eye(dim),
+                torch.eye(dim),
+                1.0
+            ),
+            "reverse": (
+                torch.eye(dim)[torch.arange(dim-1,-1,-1)],
+                torch.eye(dim)[torch.arange(dim-1,-1,-1)],
+                1.0 if dim % 4 < 2 else -1.0
+            ),
+        }[init]
+        self.register_buffer("A", initial_weight_matrix)
+        self.register_buffer("Ainv", initial_inverse)
+        self.register_buffer("detA", torch.tensor(initial_det, dtype=self.A.dtype))
         if shift:
             self.b = torch.nn.Parameter(torch.zeros(dim))
         else:
             self.register_buffer("b", torch.tensor(0.0))
-        self.register_buffer("penalty_parameter", torch.tensor(penalty_parameter))
-        self.register_buffer("min_logdet", torch.tensor(min_logdet))
-        self.register_buffer("max_logdet", torch.tensor(max_logdet))
+        self.register_buffer("min_logdet", min_logdet*torch.ones_like(self.detA))
+        self.register_buffer("max_logdet", max_logdet*torch.ones_like(self.detA))
+        self.register_buffer("penalty_buffer", torch.zeros_like(self.detA))
+        self.register_buffer("penalty_parameter", penalty_parameter*torch.ones_like(self.detA))
+
+    def _compute_scale(self, vtAinvu, detA):
+        """Returns one. I was thinking of """
+        return torch.ones_like(detA)
+
+    def _compute_products(self):
+        u = self.u
+        v = self.v
+        Ainv = self.Ainv
+        detA = self.detA
+        Ainvu = torch.mv(Ainv, u)
+        vtAinvu = torch.dot(v, Ainvu)
+        scale = self._compute_scale(vtAinvu, detA)
+        scaled_Ainvu = scale * Ainvu
+        scaled_vtAinvu = scale ** 2 * vtAinvu
+        det_update = 1 + scaled_vtAinvu
+        return scaled_Ainvu, scaled_vtAinvu, scale, det_update
+
+    #def _update_buffers(self):
+    #    self.scale = self._determine_scale(vtAinvu, self.detA)
+    #    self.det_update = 1.0 + self.scale**2 * vtAinvu
+    #    self.Ainvu = Ainvu * self.scale
 
     @staticmethod
-    def _logdet_rank_one(u, v, Ainv):
-        return torch.log(1.0+torch.einsum("i,ij,j->", v, Ainv, u))
+    def _mv_rank_one(u, v, x, scale):
+        return scale**2 * torch.einsum("i,j,...j->...i", u, v, x)
 
     @staticmethod
-    def _mv_rank_one(u, v, x):
-        return torch.einsum("i,j,...j->...i", u, v, x)
+    def _inv_rank_one(v, scale, Ainv, Ainvu, det_update):
+        vtAinv = scale * torch.einsum("i,ij->j", v, Ainv)
+        return - 1/det_update * torch.einsum("i,j", Ainvu, vtAinv)
 
     @staticmethod
-    def _inv_rank_one(u, v, Ainv):
-        vtAinv = torch.einsum("i,ij->j", v, Ainv)
-        denominator = 1 + torch.einsum("i,i->", vtAinv, u)
-        return - 1/denominator * torch.einsum("ij,j,k", Ainv, u, vtAinv)
+    def _inv_mv_rank_one(Ainvy, v, scale, Ainvu, det_update):
+        return - 1/det_update * scale * torch.einsum("i,k,...k->...i", Ainvu, v, Ainvy)
 
-    @staticmethod
-    def _inv_mv_rank_one(u, v, Ainv, Ainvy):
-        Ainvu = torch.einsum("ij,j->i", Ainv, u)
-        denominator = 1 + torch.einsum("i,i->", v, Ainvu)
-        return - 1/denominator * torch.einsum("i,k,...k->...i", Ainvu, v, Ainvy)
-
-    def pppp_merge(self):
+    def pppp_merge(self, force_merge=True):
         """PPPP update to hidden parameters."""
         with torch.no_grad():
-            self.logdetA += self._logdet_rank_one(self.u, self.v, self.Ainv)
-            self.A[:] = self.A + torch.einsum("i,j->ij", self.u, self.v)
-            self.Ainv[:] = self.Ainv + self._inv_rank_one(self.u, self.v, self.Ainv)
-            self.u[:] = 0
-            self.v[:] = torch.randn(self.dim)
+            # never merge nans or infs; instead reset
+            if not torch.isfinite(torch.cat([self.u, self.v])).all():
+                self.v[:] = torch.randn(self.dim)
+                self.u[:] = 0
+                return False
+            scaled_Ainvu, scaled_vtAinvu, scale, det_update = (
+                self._compute_products()
+            )
+
+            # sanity check
+            logabsdet_update = torch.log(torch.abs(det_update))
+            logabsdet_new = torch.log(torch.abs(det_update*self.detA))
+            sane_update = True
+            sane_update = sane_update and logabsdet_update > self.min_logdet - 4
+            sane_update = sane_update and logabsdet_new > self.min_logdet - 0.5
+            sane_update = sane_update and logabsdet_new < self.max_logdet + 0.5
+            #print("{:10.4} {:10.4} {}".format(det_update.item(), self.detA.item(), sane_update))
+            if not sane_update:
+                print("NOT SANE", logabsdet_update.item(), logabsdet_new.item())
+            if sane_update or force_merge:
+                self.detA *= det_update
+                self.A[:] = self.A + scale**2 * torch.einsum("i,j->ij", self.u, self.v)
+                self.Ainv[:] = self.Ainv + self._inv_rank_one(self.v, scale, self.Ainv, scaled_Ainvu, det_update)
+                self.v[:] = torch.randn(self.dim)
+                self.u[:] = 0
+                #if det_update < 0:
+                #if not sane_update:
+                #    self.correct()
+                return True
+            else:
+                return False
+
+    def _buffer_penalty(self, a, b):
+        if torch.isclose(self.penalty_parameter, torch.zeros_like(self.penalty_parameter)):
+            return self.penalty_parameter
+        else:
+            self.penalty_buffer = self.penalty_parameter * (
+                self._penalty(
+                    torch.log(torch.abs(a)),
+                    sigma_left=self.min_logdet,
+                    sigma_right=self.max_logdet)
+                +
+                self._penalty(
+                    torch.log(torch.abs(b)),
+                    sigma_left=self.min_logdet,
+                    sigma_right=self.max_logdet)
+            )
 
     def _forward(self, x, **kwargs):
         """Forward transform.
@@ -207,10 +286,15 @@ class InvertiblePPPP(Flow):
         dlogp : torch.tensor
             natural log of the Jacobian determinant
         """
-        new_logdet = self.logdetA + self._logdet_rank_one(self.u, self.v, self.Ainv)
-        #print(new_logdet)
-        dlogp = torch.ones_like(x[...,0,None]) * new_logdet
-        y = torch.einsum("ij,...j->...i", self.A, x) + self._mv_rank_one(self.u, self.v, x)
+        if self.training:
+            scaled_Ainvu, scaled_vtAinvu, scale, det_update = self._compute_products()
+            new_detA = self.detA * det_update
+            dlogp = torch.ones_like(x[...,0,None]) * torch.log(torch.abs(new_detA))
+            y = torch.einsum("ij,...j->...i", self.A, x) + self._mv_rank_one(self.u, self.v, x, scale)
+            self._buffer_penalty(det_update, new_detA)
+        else:
+            dlogp = torch.ones_like(x[..., 0, None]) * torch.log(torch.abs(self.detA))
+            y = torch.einsum("ij,...j->...i", self.A, x)
         return y + self.b, dlogp
 
     def _inverse(self, y, **kwargs):
@@ -230,9 +314,16 @@ class InvertiblePPPP(Flow):
         dlogp : torch.tensor
             natural log of the Jacobian determinant
         """
-        dlogp = - torch.ones_like(y[..., 0, None]) * (self.logdetA + self._logdet_rank_one(self.u, self.v, self.Ainv))
-        Ainvy = torch.einsum("ij,...j->...i", self.Ainv, y - self.b)
-        x = Ainvy + self._inv_mv_rank_one(self.u, self.v, self.Ainv, Ainvy)
+        if self.training:
+            scaled_Ainvu, scaled_vtAinvu, scale, det_update = self._compute_products()
+            new_detA = self.detA * det_update
+            dlogp = - torch.ones_like(y[..., 0, None]) * torch.log(torch.abs(new_detA))
+            Ainvy = torch.einsum("ij,...j->...i", self.Ainv, y - self.b)
+            x = Ainvy + self._inv_mv_rank_one(Ainvy, self.v, scale, scaled_Ainvu, det_update)
+            self._buffer_penalty(det_update, new_detA)
+        else:
+            dlogp = - torch.ones_like(y[..., 0, None]) * torch.log(torch.abs(self.detA))
+            x = torch.einsum("ij,...j->...i", self.Ainv, y - self.b)
         return x, dlogp
 
     def penalty(self):
@@ -245,13 +336,79 @@ class InvertiblePPPP(Flow):
         penalty : float
             Value of the penalty function
         """
-        # unnecessary to compute this twice; make a buffer
-        new_logdet = self.logdetA + self._logdet_rank_one(self.u, self.v, self.Ainv)
-        if new_logdet > self.max_logdet:
-            return self.penalty_parameter*(new_logdet - self.max_logdet)**2
-        elif new_logdet < self.min_logdet:
-            return self.penalty_parameter*(self.min_logdet - new_logdet)**2
+        if torch.isnan(self.penalty_buffer):
+            return torch.ones_like(self.penalty_buffer) * 1e8
         else:
-            return torch.zeros_like(self.penalty_parameter)
-        #else:
-        #    return self.penalty_parameter / torch.exp(new_logdet) - self.penalty_parameter / self.min_determinant
+            return self.penalty_buffer
+
+    def correct(self):
+        before = torch.mm(self.A, self.Ainv)
+        error_before = torch.norm(before - torch.eye(self.dim)).item()
+        with torch.no_grad():
+            self.Ainv[:] = _iterative_solve(self.A, self.Ainv)
+                #self.Ainv[:] = torch.inverse(self.A)
+            after = torch.norm(torch.mm(self.A, self.Ainv) - torch.eye(self.dim))
+            #if torch.norm(before - torch.eye(self.dim)).item() > 1e-3:
+            print(f"{error_before:10.7f} -> {after.item():10.7f}")
+            #olddet = torch.det(self.A)
+            #self.detA = torch.det(self.A)
+            #print(f"{olddet.item():10.7f} -> {self.detA.item():10.7f}")
+
+    @staticmethod
+    def _penalty(x, sigma_left=None, sigma_right=None):
+        result = torch.zeros_like(x)
+        if sigma_left is not None:
+            xprime = torch.relu(sigma_left - x)
+            result += xprime ** 2
+        if sigma_right is not None:
+            assert sigma_right > 0
+            xprime = torch.relu(x - sigma_right)
+            result += xprime ** 2
+        return result
+
+    def train(self, mode):
+        if torch.norm(self.u) + torch.norm(self.v) > 1e-10:
+            self.pppp_merge(force_merge=True)
+        return super().train(mode)
+
+
+class PPPPScheduler:
+    def __init__(self, model, optimizer, n_merge=10, n_force_merge=100, n_correct=500, n_correct_steps=1):
+        self.model = model
+        self.optimizer = optimizer
+        self.n_merge = n_merge
+        self.n_force_merge = n_force_merge
+        self.n_correct = n_correct
+        self.n_correct_steps = n_correct_steps
+        self.i = 0
+
+    def step(self):
+        self.i += 1
+        if self.i % self.n_merge == 0:
+            self.model.pppp_merge(force_merge=self.i % self.n_force_merge == 0)
+            self.optimizer.state = defaultdict(dict)
+        if self.i % self.n_correct == 0:
+            for _ in range(self.n_correct_steps):
+                self.model.correct()
+
+
+_iterative_solve_coefficients = {
+    2: (-1., -2.),
+    3: (1., 3., -3.),
+    7: (1./16., 120., -393., 735., -861., 651., -315., 93., -15.)
+}
+
+
+def _iterative_solve(matrix, inverse_guess, order=7):
+    """see Soleymani, https://doi.org/10.1155/2012/134653"""
+    coeffs = _iterative_solve_coefficients[order]
+    factor = coeffs[:2]
+    coeffs = coeffs[2:]
+    error = torch.mm(matrix, inverse_guess)
+    correction = error.clone()
+    indices = torch.arange(matrix.shape[0])
+    for c in reversed(coeffs):
+        correction[indices, indices] += c
+        correction = torch.mm(error, correction)
+    correction[indices, indices] += factor[1]
+    return factor[0] * torch.mm(inverse_guess, correction)
--- tests/nn/flow/test_pppp.py
+++ tests/nn/flow/test_pppp.py
@@ -1,7 +1,7 @@
 
 import pytest
 import torch
-from bgtorch.nn.flow.pppp import OrthogonalPPPP, InvertiblePPPP
+from bgtorch.nn.flow.pppp import OrthogonalPPPP, InvertiblePPPP, _iterative_solve
 
 
 def test_orthogonal_pppp():
@@ -33,7 +33,8 @@ def test_invertible_pppp():
     flow.v[:] = torch.tensor([0.1, 0.2])
     y, logdet = flow.forward(x)
 
-    x2, logdetinv = flow._inverse(y, inverse=True)
+
+    x2, logdetinv = flow.forward(y, inverse=True)
     assert torch.isclose(x2, x, atol=1e-5).all()
 
     assert(flow.penalty() > -1)
@@ -48,3 +49,31 @@ def test_invertible_pppp():
     assert torch.isclose(logdet, -logdetinv2, atol=1e-5)
     assert torch.isclose(y, y2, atol=1e-5).all()
     assert torch.isclose(flow.u, torch.zeros(1), atol=1e-5).all()
+
+    # test training mode
+    flow.train(False)
+    y_test, logdet_test = flow.forward(x)
+    assert torch.isclose(y, y_test, atol=1e-5).all()
+    assert torch.isclose(logdet, logdet_test, atol=1e-5)
+    x_test, logdetinv_test = flow.forward(y, inverse=True)
+    assert torch.isclose(x, x_test, atol=1e-5).all()
+    assert torch.isclose(-logdet, logdetinv_test, atol=1e-5)
+
+
+@pytest.mark.parametrize("mode", ["eye", "reverse"])
+@pytest.mark.parametrize("dim", [1, 3, 5, 6, 7, 10, 20])
+def test_initialization(mode, dim):
+    flow = InvertiblePPPP(dim, init=mode)
+    assert flow.A.inverse().numpy() == pytest.approx(flow.Ainv.numpy())
+    assert torch.det(flow.A).item() == pytest.approx(flow.detA.item())
+
+
+@pytest.mark.parametrize("order", [2, 3, 7])
+def test_iterative_solve(order):
+    torch.seed = 0
+    a = torch.eye(3)+0.05*torch.randn(3, 3)
+    inv = torch.inverse(a) + 0.2*torch.randn(3,3)
+    for i in range(10):
+        inv = _iterative_solve(a, inv, order)
+    assert torch.mm(inv, a).numpy() == pytest.approx(torch.eye(3).numpy(), abs=1e-5)
+
