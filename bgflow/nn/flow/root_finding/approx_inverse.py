671636e939331c3ae80d3dab286f797ac4d4e6dd
Andreas Krämer <kraemer.research@gmail.com>
2021-05-17 13:26:24 +0200

    ...

--- bgflow/nn/flow/root_finding/approx_inverse.py
+++ bgflow/nn/flow/root_finding/approx_inverse.py
@@ -263,7 +263,7 @@ class GridInversion(Transformer):
         def _residual(inp):
             extra_dims = len(out.shape) - len(cond.shape)
             out_pred, dlogp = self._transformer(
-                cond.view(*np.ones(extra_dims, dtype=int), *cond.shape),#.repeat(*inp.shape[extra_dims], torch.ones_like(cond.shape)),
+                cond.view(*np.ones(extra_dims, dtype=int), *cond.shape).repeat(*inp.shape[extra_dims], *(1 for _ in cond.shape)),
                 inp,
                 *args,
                 **kwargs
