fc684d1667160750e34fb7c409d40b5dda51c396
Andreas Krämer <kraemer.research@gmail.com>
2021-06-16 15:26:51 +0200

    conftest for ala2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 
+import os
+from types import SimpleNamespace
 import pytest
+import numpy as np
 import torch
+from bgflow import MixedCoordinateTransformation, OpenMMBridge, OpenMMEnergy
 
 
 @pytest.fixture(
@@ -31,8 +35,75 @@ def ctx(dtype, device):
     return {"dtype": dtype, "device": device}
 
 
-@pytest.yield_fixture(params=[torch.enable_grad, torch.no_grad])
+@pytest.fixture(params=[torch.enable_grad, torch.no_grad])
 def with_grad_and_no_grad(request):
     """Run a test with and without torch grad enabled"""
     with request.param():
         yield
+
+
+@pytest.fixture(scope="session")
+def ala2():
+    """Mock bgmol dataset."""
+    mm = pytest.importorskip("simtk.openmm")
+    md = pytest.importorskip("mdtraj")
+    pdb = mm.app.PDBFile(os.path.join(os.path.dirname(__file__), "data/alanine-dipeptide-nowater.pdb"))
+    system = SimpleNamespace()
+    system.topology = pdb.getTopology()
+    system.mdtraj_topology = md.Topology.from_openmm(system.topology)
+    system.system = mm.app.ForceField("amber99sbildn.xml").createSystem(
+        pdb.getTopology(),
+        removeCMMotion=True,
+        nonbondedMethod=mm.app.NoCutoff,
+        constraints=mm.app.HBonds,
+        rigidWater=True
+    )
+    system.energy_model = OpenMMEnergy(
+        66,
+        OpenMMBridge(
+            system.system,
+            mm.LangevinIntegrator(300, 1, 0.001),
+            n_workers=1
+        )
+    )
+    system.positions = pdb.getPositions()
+    system.rigid_block = np.array([6, 8, 9, 10, 14])
+    system.z_matrix = np.array([
+            [0, 1, 4, 6],
+            [1, 4, 6, 8],
+            [2, 1, 4, 0],
+            [3, 1, 4, 0],
+            [4, 6, 8, 14],
+            [5, 4, 6, 8],
+            [7, 6, 8, 4],
+            [11, 10, 8, 6],
+            [12, 10, 8, 11],
+            [13, 10, 8, 11],
+            [15, 14, 8, 16],
+            [16, 14, 8, 6],
+            [17, 16, 14, 15],
+            [18, 16, 14, 8],
+            [19, 18, 16, 14],
+            [20, 18, 16, 19],
+            [21, 18, 16, 19]
+        ])
+    dataset = SimpleNamespace()
+    dataset.system = system
+    # super-short simulation
+    xyz = []
+    simulation = mm.app.Simulation(system.topology, system.system, mm.LangevinIntegrator(300,1,0.001))
+    simulation.context.setPositions(system.positions)
+    for i in range(100):
+        simulation.step(10)
+        pos = simulation.context.getState(getPositions=True).getPositions(asNumpy=True)
+        xyz.append(pos._value)
+    dataset.xyz = np.stack(xyz, axis=0)
+    return dataset
+
+
+@pytest.fixture()
+def crd_trafo(ala2, ctx):
+    z_matrix = ala2.system.z_matrix
+    fixed_atoms = ala2.system.rigid_block
+    crd_transform = MixedCoordinateTransformation(torch.tensor(ala2.xyz, **ctx), z_matrix, fixed_atoms)
+    return crd_transform
