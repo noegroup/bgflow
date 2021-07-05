fixed bug in autograd jacobian

bgflow/utils/autograd.py
@@ -118,7 +118,7 @@ def get_jacobian(fun, x):
     z.requires_grad_(True)
     y = fun(z)
     out_grad = torch.eye(d, device=x.device, dtype=x.dtype).tile(n, 1)
-    j = torch.autograd.grad(y, z, out_grad, create_graph=True)[0].view(*shape, d, d)
+    j = torch.autograd.grad(y, z, out_grad, create_graph=True, retain_graph=True)[0].view(*shape, d, d)
     return Jacobian(
         y=einops.rearrange(y, "(n i) j -> n i j", i=d)[:, 0, :].view(*shape, -1),
         jac=j