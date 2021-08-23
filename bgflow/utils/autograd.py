b8a179edc921f6699759656e840a340ec068493d
Jonas Köhler <4534141+jonkhler@users.noreply.github.com>
2021-08-23 18:32:25 +0200

    Global IC fixed with Euler angles (#9)
    * fix batch jacobian
    
    * add fix of reference systme transformation
    
    * fix broken api; added docstring
    
    * more docstring
    
    * correct jacobian; euler angle representation
    
    * remove unused imports
    
    * faster jacobian; fixed beta euler angle bug
    
    * correct test for changed beta euler angle
    
    * added einops dependecy to CI
    
    * added einops dependency to README
    
    * added einops to conda CI ...
    
    * removed unused variable
    
    * fixed docstring errors
    
    Co-authored-by: Jonas Köhler <jonas.koehler.ks@gmail.com>
    Co-authored-by: Jonas Köhler <4534141+jonas-koehler@users.noreply.github.com>

--- bgflow/utils/autograd.py
+++ bgflow/utils/autograd.py
@@ -1,5 +1,9 @@
 import torch
 
+import einops
+from collections import namedtuple
+
+
 
 def brute_force_jacobian_trace(y, x):
     """
@@ -62,7 +66,6 @@ def brute_force_jacobian(y, x):
     TODO
     """
     output_dim = y.shape[-1]
-    input_dim = x.shape[-1]
     rows = []
     for i in range(output_dim):
         row = torch.autograd.grad(
@@ -72,8 +75,8 @@ def brute_force_jacobian(y, x):
             create_graph=True,
             retain_graph=True,
         )[0]
-        rows.append(row.view(-1, 1, input_dim))
-    jac = torch.cat(rows, dim=-2)
+        rows.append(row)
+    jac = torch.stack(rows, dim=-2)
     return jac
 
 
@@ -101,3 +104,22 @@ def batch_jacobian(y, x):
     jac = torch.stack(jac, dim=1)
 
     return jac
+
+
+Jacobian = namedtuple("Jacobian", ["y", "jac"])
+
+
+def get_jacobian(fun, x):
+    shape = x.shape[:-1]
+    d = x.shape[-1]
+    x = x.view(-1, d)
+    n = x.shape[0]
+    z = einops.repeat(x, "n j -> (n i) j", i=d)
+    z.requires_grad_(True)
+    y = fun(z)
+    out_grad = torch.eye(d, device=x.device, dtype=x.dtype).tile(n, 1)
+    j = torch.autograd.grad(y, z, out_grad, create_graph=True, retain_graph=True)[0].view(*shape, d, d)
+    return Jacobian(
+        y=einops.rearrange(y, "(n i) j -> n i j", i=d)[:, 0, :].view(*shape, -1),
+        jac=j
+    )
