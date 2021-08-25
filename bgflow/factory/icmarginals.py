edb149a98e2e40004f6ea7e2dbfce5ede499932b
Andreas Krämer <kraemer.research@gmail.com>
2021-08-25 23:44:24 +0200

    inform marginals with data and FF

--- bgflow/factory/icmarginals.py
+++ bgflow/factory/icmarginals.py
@@ -33,6 +33,8 @@ class InternalCoordinateMarginals(dict):
             augmented=AUGMENTED,
     ):
         self.ctx = ctx
+        self.current_dims = current_dims
+
         super().__init__()
         if bonds in current_dims:
             self[bonds] = TruncatedNormalDistribution(
@@ -75,17 +77,29 @@ class InternalCoordinateMarginals(dict):
     def __eq__(self, other):
         return super().__eq__(other) and (other.ctx == self.ctx)
 
-    def inform_with_force_field(self, system, coordinate_transform, temperature):
-        return NotImplemented
-        #bond_lengths, force_constants = lookup_bonds(system, coordinate_transform.bond_indices, temperature)
-        #angles, force_constants = lookup_angles(system, coordinate_transform.angle_indices, temperature)
+    def inform_with_force_field(
+            self,
+            system,
+            coordinate_transform,
+            temperature,
+            bonds=BONDS,
+            angles=ANGLES,
+            torsions=TORSIONS
+    ):
+        import bgmol
+        if bonds in self.current_dims:
+            self[bonds] = bgmol.bond_marginal_estimate(
+                system, coordinate_transform, temperature, **self.ctx
+            )
+        if angles in self.current_dims:
+            self[angles] = bgmol.angle_marginal_estimate(
+                system, coordinate_transform, temperature, **self.ctx
+            )
 
     def inform_with_data(
             self,
             data,
             coordinate_transform,
-            current_dims,
-            ctx,
             bond_lower=1e-5,
             bond_upper=np.infty,
             angle_lower=1e-5,
@@ -93,30 +107,34 @@ class InternalCoordinateMarginals(dict):
             torsion_lower=0.0,
             torsion_upper=1.0,
             torsion_bins=50,
+            constrained_bond_indices=None,
             bonds=BONDS,
             angles=ANGLES,
             torsions=TORSIONS,
     ):
         bond_values, angle_values, torsion_values, *_ = coordinate_transform.forward(data)
 
-        if bonds in current_dims:
-            bond_mu = bond_values.mean(axis=-1)
-            bond_sigma = bond_values.std(axis=-1)
+        if bonds in self.current_dims:
+            bond_mu = bond_values.mean(axis=0)
+            bond_sigma = bond_values.std(axis=0)
+            if constrained_bond_indices is not None:
+                bond_mu = bond_mu[constrained_bond_indices]
+                bond_sigma = bond_sigma[constrained_bond_indices]
             self[bonds] = TruncatedNormalDistribution(
-                mu=bond_mu * torch.ones(current_dims[bonds], **ctx),
-                sigma=bond_sigma * torch.ones(current_dims[bonds], **ctx),
-                lower_bound=torch.tensor(bond_lower, **ctx),
-                upper_bound=torch.tensor(bond_upper, **ctx),
+                mu=torch.tensor(bond_mu, **self.ctx),
+                sigma=torch.tensor(bond_sigma, **self.ctx),
+                lower_bound=torch.tensor(bond_lower, **self.ctx),
+                upper_bound=torch.tensor(bond_upper, **self.ctx),
             )
 
-        if angles in current_dims:
-            angle_mu = angle_values.mean(axis=-1)
-            angle_sigma = angle_values.std(axis=-1)
+        if angles in self.current_dims:
+            angle_mu = angle_values.mean(axis=0)
+            angle_sigma = angle_values.std(axis=0)
             self[angles] = TruncatedNormalDistribution(
-                mu=angle_mu * torch.ones(current_dims[angles], **ctx),
-                sigma=angle_sigma * torch.ones(current_dims[angles], **ctx),
-                lower_bound=torch.tensor(angle_lower, **ctx),
-                upper_bound=torch.tensor(angle_upper, **ctx),
+                mu=torch.tensor(angle_mu, **self.ctx),
+                sigma=torch.tensor(angle_sigma, **self.ctx),
+                lower_bound=torch.tensor(angle_lower, **self.ctx),
+                upper_bound=torch.tensor(angle_upper, **self.ctx),
             )
 
         torsion_values = torsion_values.detach().cpu().numpy()
