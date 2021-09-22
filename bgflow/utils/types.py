a08dec0c07c01a664dc710e7fa433cbd95ebc78a
Andreas Krämer <kraemer.research@gmail.com>
2021-09-22 17:38:47 +0200

    as_numpy util function

--- bgflow/utils/types.py
+++ bgflow/utils/types.py
@@ -2,9 +2,13 @@ import numpy as np
 import torch
 
 
+__all__ = ["is_list_or_tuple", "assert_numpy", "as_numpy"]
+
+
 def is_list_or_tuple(x):
     return isinstance(x, list) or isinstance(x, tuple)
-       
+
+
 def assert_numpy(x, arr_type=None):
     if isinstance(x, torch.Tensor):
         if x.is_cuda:
@@ -16,3 +20,8 @@ def assert_numpy(x, arr_type=None):
     if arr_type is not None:
         x = x.astype(arr_type)
     return x
+
+
+def as_numpy(tensor):
+    """convert tensor to numpy"""
+    return torch.as_tensor(tensor).detach().cpu().numpy()
