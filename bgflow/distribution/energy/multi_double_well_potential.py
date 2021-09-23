d7e3e87329980892180c38f6da2e584d3a905929
LeonKein <leon.klein.2601@gmail.com>
2021-09-23 16:58:33 +0200

    Test and refactor MultiDoubleWellPotential

--- bgflow/distribution/energy/multi_double_well_potential.py
+++ bgflow/distribution/energy/multi_double_well_potential.py
@@ -1,47 +1,41 @@
 from .base import Energy
-from bgflow.utils import distance_vectors, distances_from_vectors
-import torch
-
+from bgflow.utils import compute_distances
 
 __all__ = ["MultiDoubleWellPotential"]
 
 
 class MultiDoubleWellPotential(Energy):
-    
+    """Energy for a many particle system with pair wise double-well interactions.
+    The energy of the double-well is given via
+    .. math:: E_{DW}(d) = a * (d-offset)^4 + b * (d-offset)^2 + c.
+
+    Parameters
+    ----------
+    dim : int
+        Number of degrees of freedom ( = space dimension x n_particles)
+    n_particles : int
+        Number of Lennard-Jones particles
+    a, b, c, offset : float
+        parameters of the potential
+    """
+
     def __init__(self, dim, n_particles, a, b, c, offset):
         super().__init__(dim)
         self._dim = dim
         self._n_particles = n_particles
+        self._n_dimensions = dim // n_particles
         self._a = a
         self._b = b
         self._c = c
         self._offset = offset
-        
+
     @property
     def dim(self):
         return self._dim
-    
-    def energy_dw(self, x):
-        n_batch = x.shape[0]
-        x = x.view(n_batch, self._n_particles, -1)
 
-        dists = distances_from_vectors(
-            distance_vectors(x.view(n_batch, self._n_particles, -1))
-        )
-        dists = dists.view(-1, 1)
-
+    def _energy(self, x):
+        dists = compute_distances(x, self._n_particles, self._n_dimensions)
         dists = dists - self._offset
 
-        energies = self._a * dists**4 + self._b * dists**2 + self._c
-        return energies.view(n_batch, -1).sum(-1) / 2
-        
-    def _energy(self, x, temperature=None):
-        assert x.shape[-1] == self._dim, "`x` does not match `dim`"
-        if temperature is None:
-            temperature = 1.
-        return self.energy_dw(x).view(-1, 1) / temperature
-    
-    def force(self, x, temperature=None):
-        x = x.requires_grad_(True)
-        e = self._energy(x, temperature=temperature)
-        return -torch.autograd.grad(e.sum(), x)[0]
+        energies = self._a * dists ** 4 + self._b * dists ** 2 + self._c
+        return energies.sum(-1, keepdim=True)
