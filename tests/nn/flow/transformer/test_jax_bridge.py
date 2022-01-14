fixed tests to fail numerical precision for random seeds

tests/nn/flow/transformer/test_jax_bridge.py
@@ -10,6 +10,7 @@ except ImportError:
     jax2torch = None
 
 import pytest
+import numpy as np
 import torch
 from bgflow.nn.flow.transformer.jax_bridge import (
     chain,
@@ -38,17 +39,23 @@ def test_simple_mixture_transformer():
         smooth_ramp)
     forward, inverse = to_bgflow(bijector)
 
-    batch = 128
-    features = 64
-    num_components = 16
+    np.random.seed(43)
 
-    x = torch.rand(batch, features)
-    weights = torch.randn(batch, features, num_components)
-    shift = torch.randn(batch, features, num_components)
-    scale = torch.randn(batch, features, num_components)
-    mix = torch.randn(batch, features, num_components)
+    batch = 17
+    features = 19
+    num_components = 7
+
+    x = torch.from_numpy(np.random.uniform(size=(batch, features))).float()
+    weights = torch.from_numpy(
+        np.random.normal(size=(batch, features, num_components))).float()
+    shift = torch.from_numpy(
+        np.random.normal(size=(batch, features, num_components))).float()
+    scale = torch.from_numpy(
+        np.random.normal(size=(batch, features, num_components))).float()
+    mix = torch.from_numpy(
+        np.random.normal(size=(batch, features, num_components))).float()
     y, ldjy = forward(x, (weights, shift, scale, mix))
     z, ldjz = inverse(y, (weights, shift, scale, mix))
-    assert torch.allclose(x, z, rtol=1e-4, atol=1e-3)
+    assert torch.allclose(x, z, rtol=1e-4, atol=1e-4)
     assert torch.allclose(ldjy + ldjz, torch.zeros_like(ldjz),
-                          rtol=1e-4, atol=1e-3)
+                          rtol=1e-4, atol=1e-4)