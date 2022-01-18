400875f9bae664e76dc02689c4f1f609463ed068
Jonas Köhler <jonas.koehler.ks@gmail.com>
2022-01-18 16:08:21 +0100

    make params consistent

--- bgflow/nn/flow/transformer/jax.py
+++ bgflow/nn/flow/transformer/jax.py
@@ -32,8 +32,10 @@ def monomial_ramp(x, order=2):
 
 def ramp_to_sigmoid(ramp):
     """Generalized sigmoid, given a ramp."""
-    def _sigmoid(x):
-        return ramp(x) / (ramp(x) + ramp(1. - x))
+    def _sigmoid(x, *params):
+        numer = ramp(x, *params)
+        denom = numer + ramp(1. - x, *params)
+        return numer / denom
     return _sigmoid
 
 
@@ -41,45 +43,43 @@ def affine_sigmoid(sigmoid, eps=1e-8):
     """Generalized affine sigmoid transform."""
     assert eps > 0
 
-    def _affine_sigmoid(x, shift, slope, mix):
+    def _affine_sigmoid(x, shift, slope, mix, *params):
         slope = jnp.exp(slope)
         mix = jax.nn.sigmoid(mix) * (1. - eps) + eps
-        return (mix * sigmoid(slope * (x - shift))
+        return (mix * sigmoid(slope * (x - shift), *params)
                 + (1. - mix) * x)
     return _affine_sigmoid
 
 
-def wrap_sigmoid(sigmoid, sheaves=None):
+def wrap_around(bijector, sheaves=None):
     """Wraps affine sigmoid around circle."""
     if sheaves is None:
         sheaves = jnp.array([-1, 0, 1])
-    wrapped = mixture(sigmoid)
 
-    def _wrapped_sigmoid(x, shift, slope, mix):
+    def _wrapped(x, *params):
         weights = jnp.zeros_like(sheaves)
-        shift = shift + sheaves
-        slope = jnp.tile(slope, [len(sheaves)])
-        mix = jnp.tile(mix, [len(sheaves)])
-        return wrapped(x, weights, shift, slope, mix)
-    return _wrapped_sigmoid
+        x = x - sheaves
+        params = [jnp.tile(p, [len(sheaves)]) for p in params]
+        return mixture(bijector)(x, *params)
+    return _wrapped
 
 
 def remap_to_unit(fun):
     """Maps transformation back to [0, 1]."""
     @functools.wraps(fun)
-    def _remapped(x, *args):
-        y1 = fun(jnp.ones_like(x), *args)
-        y0 = fun(jnp.zeros_like(x), *args)
+    def _remapped(x, *params):
+        y1 = fun(jnp.ones_like(x), *params)
+        y0 = fun(jnp.zeros_like(x), *params)
         return (fun(x, *args) - y0) / (y1 - y0)
     return _remapped
 
 
 def mixture(bijector):
     """Combines multiple bijectors into a mixture."""
-    def _mixture_bijector(x, weights, *mixture_conds):
+    def _mixture_bijector(x, weights, *params):
         components = jax.vmap(
             functools.partial(bijector, x),
             in_axes=-1,
-            out_axes=-1)(*mixture_conds)
+            out_axes=-1)(*params)
         return jnp.sum(jax.nn.softmax(weights) * components)
     return _mixture_bijector
