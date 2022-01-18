6d4fcf630cb9fba99aa667fe510f63ec6ee03b61
Andreas Krämer <kraemer.research@gmail.com>
2022-01-18 14:07:34 +0100

    test on cuda/float64

--- tests/nn/flow/transformer/test_jax_bridge.py
+++ tests/nn/flow/transformer/test_jax_bridge.py
@@ -30,7 +30,7 @@ from bgflow.nn.flow.transformer.jax import (
                     reason='skipping test due missing jax installation')
 @pytest.mark.skipif(jax2torch is None,
                     reason='skipping test due to missing jax2torch installation')
-def test_simple_mixture_transformer():
+def test_simple_mixture_transformer(ctx):
     bijector = chain(
         mixture,
         remap_to_unit,
@@ -45,15 +45,15 @@ def test_simple_mixture_transformer():
     features = 19
     num_components = 7
 
-    x = torch.from_numpy(np.random.uniform(size=(batch, features))).float()
+    x = torch.from_numpy(np.random.uniform(size=(batch, features))).to(**ctx)
     weights = torch.from_numpy(
-        np.random.normal(size=(batch, features, num_components))).float()
+        np.random.normal(size=(batch, features, num_components))).to(**ctx)
     shift = torch.from_numpy(
-        np.random.normal(size=(batch, features, num_components))).float()
+        np.random.normal(size=(batch, features, num_components))).to(**ctx)
     scale = torch.from_numpy(
-        np.random.normal(size=(batch, features, num_components))).float()
+        np.random.normal(size=(batch, features, num_components))).to(**ctx)
     mix = torch.from_numpy(
-        np.random.normal(size=(batch, features, num_components))).float()
+        np.random.normal(size=(batch, features, num_components))).to(**ctx)
     y, ldjy = forward(x, (weights, shift, scale, mix))
     z, ldjz = inverse(y, (weights, shift, scale, mix))
     assert torch.allclose(x, z, rtol=1e-4, atol=1e-4)
