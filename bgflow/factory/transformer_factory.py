ee8393a6f800a49fb5f99adf9655d34b636ef0bb
Andreas Krämer <kraemer.research@gmail.com>
2022-02-15 18:19:18 +0100

    delete skipped test

--- bgflow/factory/transformer_factory.py
+++ bgflow/factory/transformer_factory.py
@@ -42,43 +42,42 @@ def _make_affine_transformer(what, shape_info, conditioners, **kwargs):
         **kwargs
     )
 
-
-def _make_sigmoid_transformer(
-        what,
-        shape_info,
-        conditioners,
-        smoothness_type="type1",
-        zero_boundary_left=False,
-        zero_boundary_right=False,
-        **kwargs
-):
-    assert all(field.is_circular == what[0].is_circular for field in what)
-    transformer = bg.MixtureCDFTransformer(
-        compute_weights=conditioners["weights"],
-        compute_components=bg.AffineSigmoidComponents(
-            conditional_ramp=bg.SmoothRamp(
-                compute_alpha=conditioners["alphas"],
-                unimodal=True,
-                ramp_type=smoothness_type
-            ),
-            compute_params=conditioners["params"],
-            periodic=what[0].is_circular,
-            min_density=torch.tensor(1e-6),
-            log_sigma_bound=torch.tensor(1.),
-            zero_boundary_left=zero_boundary_left,
-            zero_boundary_right=zero_boundary_right,
-            **kwargs
-        )
-    )
-    transformer = bg.WrapCDFTransformerWithInverse(
-        transformer=transformer,
-        oracle=bg.GridInversion( #bg.BisectionRootFinder(
-            transformer=transformer,
-            compute_init_grid=lambda x, y: torch.linspace(0, 1, 100).view(-1, 1, 1).repeat(1, *y.shape).to(y)
-            #abs_tol=torch.tensor(1e-5), max_iters=max_iters, verbose=False, raise_exception=True
-        )
-    )
-    return transformer
+# def _make_sigmoid_transformer(
+#         what,
+#         shape_info,
+#         conditioners,
+#         smoothness_type="type1",
+#         zero_boundary_left=False,
+#         zero_boundary_right=False,
+#         **kwargs
+# ):
+#     assert all(field.is_circular == what[0].is_circular for field in what)
+#     transformer = bg.MixtureCDFTransformer(
+#         compute_weights=conditioners["weights"],
+#         compute_components=bg.AffineSigmoidComponents(
+#             conditional_ramp=bg.SmoothRamp(
+#                 compute_alpha=conditioners["alphas"],
+#                 unimodal=True,
+#                 ramp_type=smoothness_type
+#             ),
+#             compute_params=conditioners["params"],
+#             periodic=what[0].is_circular,
+#             min_density=torch.tensor(1e-6),
+#             log_sigma_bound=torch.tensor(1.),
+#             zero_boundary_left=zero_boundary_left,
+#             zero_boundary_right=zero_boundary_right,
+#             **kwargs
+#         )
+#     )
+#     transformer = bg.WrapCDFTransformerWithInverse(
+#         transformer=transformer,
+#         oracle=bg.GridInversion( #bg.BisectionRootFinder(
+#             transformer=transformer,
+#             compute_init_grid=lambda x, y: torch.linspace(0, 1, 100).view(-1, 1, 1).repeat(1, *y.shape).to(y)
+#             #abs_tol=torch.tensor(1e-5), max_iters=max_iters, verbose=False, raise_exception=True
+#         )
+#     )
+#     return transformer
 
 
 TRANSFORMER_FACTORIES = {
--- tests/factory/test_generator_builder.py
+++ tests/factory/test_generator_builder.py
@@ -36,7 +36,7 @@ def test_builder_api(ala2, ctx):
     generator = builder.build_generator()
     # play forward and backward
     samples = generator.sample(2)
-    energy = generator.energy(samples)
+    generator.energy(samples)
     generator.kldiv(10)
 
 
@@ -60,41 +60,6 @@ def test_builder_augmentation_and_global(ala2, ctx):
     assert len(samples) == 2
     generator.energy(*samples)
     generator.kldiv(10)
-
-
-def test_builder_custom(ala2, ctx):
-    """This would be cool. Three possible implementations:
-    - keep track of indices inside the factory. since layers are added in order, this is no problem.
-    - allow selecting tensors by name in the logistical flows (coupling, split, ...). Give the
-      `Flow` class two optional attributes names_in, names_out; then either
-        - (a) just set a custom tensor._name/tensor._info attribute - no idea where this may break in the future
-        - (b) give the flow
-
-    Is it clever to have the crd_transform as the central piece to all this?
-    Or can we somehow make it more general? Maybe start from the prior.
-    """
-    pytest.skip()
-    z_matrix = ala2.system.z_matrix
-    fixed_atoms = ala2.system.rigid_block
-    crd_transform = MixedCoordinateTransformation(torch.tensor(ala2.xyz, **ctx), z_matrix, fixed_atoms)
-    shape_info = ShapeDictionary.from_coordinate_transform(crd_transform)
-    builder = BoltzmannGeneratorBuilder(shape_info)
-    builder.prior_type[BONDS] = bg.NormalDistribution
-    builder.transformer_type[BONDS] = bg.AffineTransformer
-    builder.add_condition(BONDS, on=ANGLES)
-    BONDS1 = TensorInfo("BONDS1")
-    BONDS2 = TensorInfo("BONDS2")
-    builder.add_split(BONDS, into=(BONDS1, BONDS2), sizes_or_indices=(14, 3))
-    builder.add_condition(BONDS1, on=BONDS2)
-    builder.add_merge((BONDS1, BONDS2), to=BONDS)
-
-    marginal_cdf = MarginalICTransform()
-    marginal_cdf = InformedMarginalICTransform(crd_transform, system, topology)
-    marginal_cdf.scan_data(data)
-    marginal_cdf.scan_torsions(temperature=300.)
-    builder.add_map_to_ic_domains(marginal_cdf)
-    builder.add_map_to_cartesian(crd_transform)
-    builder.build_generator()
 
 
 def test_builder_add_layer_and_param_groups(ctx):
