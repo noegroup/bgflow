d6a72aaea2a4c1152976830e1e4762bea587e35a
Michele Invernizzi <inve.michele@gmail.com>
2022-04-26 11:15:32 +0200

    added mueller potential

--- bgflow/distribution/energy/double_well.py
+++ bgflow/distribution/energy/double_well.py
@@ -3,7 +3,7 @@ import torch
 from .base import Energy
 
 
-__all__ = ["DoubleWellEnergy", "MultiDimensionalDoubleWell"]
+__all__ = ["DoubleWellEnergy", "MultiDimensionalDoubleWell", "MuellerEnergy"]
 
 
 class DoubleWellEnergy(Energy):
@@ -43,3 +43,30 @@ class MultiDimensionalDoubleWell(Energy):
             x = torch.matmul(x, self._transformer)
         e1 = self._a * x + self._b * x.pow(2) + self._c * x.pow(4)
         return e1.sum(dim=1, keepdim=True)
+
+
+class MuellerEnergy(Energy):
+    def __init__(self, dim=2, scale=0.15):
+        super().__init__(dim)
+        assert dim >= 2
+        if not isinstance(scale, torch.Tensor):
+            scale = torch.tensor(scale)
+        self._scale = scale
+
+    def _energy(self, x):
+        xx = x[..., [0]]
+        yy = x[..., [1]]
+        e1 = -200 * torch.exp(-(xx -1).pow(2) -10 * yy.pow(2))
+        e2 = -100 * torch.exp(-xx.pow(2) -10 * (yy -0.5).pow(2))
+        e3 = -170 * torch.exp(-6.5 * (0.5 + xx).pow(2) +11 * (xx +0.5) * (yy -1.5) -6.5 * (yy -1.5).pow(2))
+        e4 = 15 * torch.exp(0.7 * (1 +xx).pow(2) +0.6 * (xx +1) * (yy -1) +0.7 * (yy -1).pow(2)) +146.7
+        v = x[..., 2:]
+        ev = 0.5 * v.pow(2).sum(dim=-1, keepdim=True)
+        return self._scale * (e1 + e2 + e3 + e4) + ev
+
+    @property
+    def potential_str(self):
+      pot_str = f'{self._scale:g}*(-200*exp(-(x-1)^2-10*y^2)-100*exp(-x^2-10*(y-0.5)^2)-170*exp(-6.5*(0.5+x)^2+11*(x+0.5)*(y-1.5)-6.5*(y-1.5)^2)+15*exp(0.7*(1+x)^2+0.6*(x+1)*(y-1)+0.7*(y-1)^2)+146.7)'
+      if self.dim == 3:
+        pot_str += '+0.5*z^2'
+      return pot_str
