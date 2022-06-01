34df704bbde0c90ec3497dd757c4a4b4b9b69e95
Olllom <kraemer.research@gmail.com>
2022-06-01 15:10:35 +0200

    reenable smooth transform

--- bgflow/factory/transformer_factory.py
+++ bgflow/factory/transformer_factory.py
@@ -4,6 +4,8 @@ import torch
 from ..nn.flow.inverted import InverseFlow
 from ..nn.flow.transformer.affine import AffineTransformer
 from ..nn.flow.transformer.spline import ConditionalSplineTransformer
+from ..nn.flow.transformer.compact.compact import MixtureCDFTransformer, AffineSigmoidComponents, SmoothRamp
+from ..nn.flow.root_finding.approx_inverse import GridInversion, WrapCDFTransformerWithInverse
 
 __all__ = ["make_transformer"]
 
@@ -42,47 +44,48 @@ def _make_affine_transformer(what, shape_info, conditioners, **kwargs):
         **kwargs
     )
 
-# def _make_sigmoid_transformer(
-#         what,
-#         shape_info,
-#         conditioners,
-#         smoothness_type="type1",
-#         zero_boundary_left=False,
-#         zero_boundary_right=False,
-#         **kwargs
-# ):
-#     assert all(field.is_circular == what[0].is_circular for field in what)
-#     transformer = bg.MixtureCDFTransformer(
-#         compute_weights=conditioners["weights"],
-#         compute_components=bg.AffineSigmoidComponents(
-#             conditional_ramp=bg.SmoothRamp(
-#                 compute_alpha=conditioners["alphas"],
-#                 unimodal=True,
-#                 ramp_type=smoothness_type
-#             ),
-#             compute_params=conditioners["params"],
-#             periodic=what[0].is_circular,
-#             min_density=torch.tensor(1e-6),
-#             log_sigma_bound=torch.tensor(1.),
-#             zero_boundary_left=zero_boundary_left,
-#             zero_boundary_right=zero_boundary_right,
-#             **kwargs
-#         )
-#     )
-#     transformer = bg.WrapCDFTransformerWithInverse(
-#         transformer=transformer,
-#         oracle=bg.GridInversion( #bg.BisectionRootFinder(
-#             transformer=transformer,
-#             compute_init_grid=lambda x, y: torch.linspace(0, 1, 100).view(-1, 1, 1).repeat(1, *y.shape).to(y)
-#             #abs_tol=torch.tensor(1e-5), max_iters=max_iters, verbose=False, raise_exception=True
-#         )
-#     )
-#     return transformer
+
+def _make_sigmoid_transformer(
+        what,
+        shape_info,
+        conditioners,
+        smoothness_type="type1",
+        zero_boundary_left=False,
+        zero_boundary_right=False,
+        **kwargs
+):
+    assert all(field.is_circular == what[0].is_circular for field in what)
+    transformer = MixtureCDFTransformer(
+        compute_weights=conditioners["weights"],
+        compute_components=AffineSigmoidComponents(
+            conditional_ramp=SmoothRamp(
+                compute_alpha=conditioners["alphas"],
+                unimodal=True,
+                ramp_type=smoothness_type
+            ),
+            compute_params=conditioners["params"],
+            periodic=what[0].is_circular,
+            min_density=torch.tensor(1e-6),
+            log_sigma_bound=torch.tensor(1.),
+            zero_boundary_left=zero_boundary_left,
+            zero_boundary_right=zero_boundary_right,
+            **kwargs
+        )
+    )
+    transformer = WrapCDFTransformerWithInverse(
+        transformer=transformer,
+        oracle=GridInversion( #bg.BisectionRootFinder(
+            transformer=transformer,
+            compute_init_grid=lambda x, y: torch.linspace(0, 1, 100).view(-1, 1, 1).repeat(1, *y.shape).to(y)
+            #abs_tol=torch.tensor(1e-5), max_iters=max_iters, verbose=False, raise_exception=True
+        )
+    )
+    return transformer
 
 
 TRANSFORMER_FACTORIES = {
     ConditionalSplineTransformer: _make_spline_transformer,
     AffineTransformer: _make_affine_transformer,
-    # MixtureCDFTransformer: _make_sigmoid_transformer
+    MixtureCDFTransformer: _make_sigmoid_transformer
 }
 
