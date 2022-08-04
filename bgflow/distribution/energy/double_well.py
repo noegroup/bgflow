302ac5a5cce7afe7ebbed37a6888cb39610476ec
Michele Invernizzi <inve.michele@gmail.com>
2022-08-04 16:20:07 +0200

    fix typo

--- bgflow/distribution/energy/double_well.py
+++ bgflow/distribution/energy/double_well.py
@@ -1,9 +1,10 @@
 import torch
 
 from .base import Energy
+from numpy import pi as PI
 
 
-__all__ = ["DoubleWellEnergy", "MultiDimensionalDoubleWell", "MuellerEnergy"]
+__all__ = ["DoubleWellEnergy", "MultiDimensionalDoubleWell", "MuellerEnergy", "ModifiedWolfeQuapp"]
 
 
 class DoubleWellEnergy(Energy):
@@ -46,12 +47,12 @@ class MultiDimensionalDoubleWell(Energy):
 
 
 class MuellerEnergy(Energy):
-    def __init__(self, dim=2, scale=0.15):
+    def __init__(self, dim=2, scale1=0.15, scale2=15, beta=1.):
         super().__init__(dim)
         assert dim >= 2
-        if not isinstance(scale, torch.Tensor):
-            scale = torch.tensor(scale)
-        self._scale = scale
+        self._scale1 = scale1
+        self._scale2 = scale2
+        self._beta = beta
 
     def _energy(self, x):
         xx = x[..., [0]]
@@ -59,14 +60,43 @@ class MuellerEnergy(Energy):
         e1 = -200 * torch.exp(-(xx -1).pow(2) -10 * yy.pow(2))
         e2 = -100 * torch.exp(-xx.pow(2) -10 * (yy -0.5).pow(2))
         e3 = -170 * torch.exp(-6.5 * (0.5 + xx).pow(2) +11 * (xx +0.5) * (yy -1.5) -6.5 * (yy -1.5).pow(2))
-        e4 = 15 * torch.exp(0.7 * (1 +xx).pow(2) +0.6 * (xx +1) * (yy -1) +0.7 * (yy -1).pow(2)) +146.7
+        e4 = 15.0 * torch.exp(0.7 * (1 +xx).pow(2) +0.6 * (xx +1) * (yy -1) +0.7 * (yy -1).pow(2)) +146.7
         v = x[..., 2:]
-        ev = 0.5 * v.pow(2).sum(dim=-1, keepdim=True)
-        return self._scale * (e1 + e2 + e3 + e4) + ev
+        ev = self._scale2 * 0.5 * v.pow(2).sum(dim=-1, keepdim=True)
+        return self._beta * (self._scale1 * (e1 + e2 + e3 + e4) + ev)
 
     @property
     def potential_str(self):
-      pot_str = f'{self._scale:g}*(-200*exp(-(x-1)^2-10*y^2)-100*exp(-x^2-10*(y-0.5)^2)-170*exp(-6.5*(0.5+x)^2+11*(x+0.5)*(y-1.5)-6.5*(y-1.5)^2)+15*exp(0.7*(1+x)^2+0.6*(x+1)*(y-1)+0.7*(y-1)^2)+146.7)'
-      if self.dim == 3:
-        pot_str += '+0.5*z^2'
+      pot_str = f'{self._scale1:g}*(-200*exp(-(x-1)^2-10*y^2)-100*exp(-x^2-10*(y-0.5)^2)-170*exp(-6.5*(0.5+x)^2+11*(x+0.5)*(y-1.5)-6.5*(y-1.5)^2)+15*exp(0.7*(1+x)^2+0.6*(x+1)*(y-1)+0.7*(y-1)^2)+146.7)'
+      if self.dim >= 3:
+        pot_str += f'+{self._scale2:g}*0.5*z^2'
+      return pot_str
+
+class ModifiedWolfeQuapp(Energy):
+    def __init__(self, dim=2, theta=-0.3*PI/2, scale1=2, scale2=15, beta=1.):
+        super().__init__(dim)
+        assert dim >= 2
+        self._scale1 = scale1
+        self._scale2 = scale2
+        self._beta = beta
+        self._c = torch.cos(torch.as_tensor(theta))
+        self._s = torch.sin(torch.as_tensor(theta))
+
+    def _energy(self, x):
+        xx = self._c * x[..., [0]] - self._s * x[..., [1]]
+        yy = self._s * x[..., [0]] + self._c * x[..., [1]]
+        e4 = xx.pow(4) + yy.pow(4)
+        e2 = -2 * xx.pow(2) - 4 * yy.pow(2) + 2 * xx * yy
+        e1 = 0.8 * xx + 0.1 * yy + 9.28
+        v = x[..., 2:]
+        ev = self._scale2 * 0.5 * v.pow(2).sum(dim=-1, keepdim=True)
+        return self._beta * (self._scale1 * (e4 + e2 + e1) + ev)
+
+    @property
+    def potential_str(self):
+      x_str = f'({self._c:g}*x-{self._s:g}*y)'
+      y_str = f'({self._s:g}*x+{self._c:g}*y)'
+      pot_str = f'{self._scale1:g}*({x_str}^4+{y_str}^4-2*{x_str}^2-4*{y_str}^2+2*{x_str}*{y_str}+0.8*{x_str}+0.1*{y_str}+9.28)'
+      if self.dim >= 3:
+        pot_str += f'+{self._scale2:g}*0.5*z^2'
       return pot_str
