b1960528a0e37e207ddff308a0b713d429502944
LeonKein <leon.klein.2601@gmail.com>
2024-01-30 17:57:17 +0100

    Add flow matching trainer

--- bgflow/nn/training/trainers.py
+++ bgflow/nn/training/trainers.py
@@ -7,7 +7,7 @@ from bgflow.utils.types import assert_numpy
 from bgflow.distribution.sampling import DataSetSampler
 
 
-__all__ = ["LossReporter", "KLTrainer"]
+__all__ = ["LossReporter", "KLTrainer", "FlowMatchingTrainer"]
 
 
 class LossReporter:
@@ -43,7 +43,6 @@ class LossReporter:
 
     def recent(self, n_recent=1):
         return np.array([raw[-n_recent:] for raw in self._raw])
-
 
 class KLTrainer(object):
     def __init__(
@@ -203,3 +202,152 @@ class KLTrainer(object):
 
     def losses(self, n_smooth=1):
         return self.reporter.losses(n_smooth=n_smooth)
+    
+
+class FlowMatchingTrainer(object):
+    def __init__(
+        self, bg, optim=None, optimal_transport=False, equivariant_optimal_transport=False, test_loss=False
+    ):
+        """Trainer for minimizing the flow matching objective. Supports also optimal transport flow matching.
+        """
+
+        self.bg = bg
+
+        if optim is None:
+            optim = torch.optim.Adam(bg.parameters(), lr=5e-4)
+        self.optim = optim
+
+        loss_names = []
+        self.optimal_transport = optimal_transport
+        self.equivariant_optimal_transport = equivariant_optimal_transport
+        assert not (self.optimal_transport and self.equivariant_optimal_transport), \
+            "Choose either OT flow matching or equivariant OT flow matching (not both)."
+        loss_names.append("FM")
+        if test_loss:
+            self.test_loss = test_loss
+            loss_names.append("FM(Test)")
+        self.reporter = LossReporter(*loss_names)
+
+
+
+    def flow_matching_loss(self, batchsize, prior_data, target_data, noise_data, noise_scaling):
+        """
+        Computes the flow matching or optimal flow matching loss. 
+        """
+        x1 = target_data.sample(batchsize)
+        t = torch.rand(batchsize, 1).to(x1)
+        x0 = prior_data.sample(batchsize)
+
+        if self.optimal_transport:
+            import ot as pot
+            # Resample x0, x1 according to transport matrix
+            a1, b1 = pot.unif(x0.size()[0]), pot.unif(x1.size()[0])
+            M = torch.cdist(x0, x1) ** 2
+            M = M / M.max()
+            pi = pot.emd(a1, b1, M.detach().cpu().numpy())
+            # Sample random interpolations on pi
+            p = pi.flatten()
+            p = p / p.sum()
+            choices = np.random.choice(pi.shape[0] * pi.shape[1], p=p, size=batchsize)
+            i, j = np.divmod(choices, pi.shape[1])
+            x0 = x0[i]
+            x1 = x1[j]
+        # evaluation points
+        mu_t = x0 * (1 - t) + x1 * t
+        noise = noise_data.sample(batchsize)
+        x = mu_t + noise_scaling * noise
+        # target vectorfield
+        ut = x1 - x0
+        vt = self.bg.flow._dynamics._dynamics._dynamics_function(t, x)
+        loss = torch.mean((vt - ut) ** 2)                  
+        return loss
+    
+    def train(
+        self,
+        n_iter,
+        prior_data=None,
+        target_data=None,
+        test_prior_data=None,
+        test_target_data=None,
+        noise_data=None,
+        noise_scaling=0.01,
+        batchsize=128,
+        n_print=0,
+        schedulers=(),
+        progress_bar=lambda x:x
+    ):
+        """
+        Train the network.
+
+        Parameters
+        ----------
+        n_iter : int
+            Number of training iterations.
+        prior_data : torch.Tensor or Sampler
+            Training data (prior)
+        target_data : torch.Tensor or Sampler
+            Training data
+        test_prior_data : torch.Tensor or Sampler
+            Test data (target)
+        test_target_data : torch.Tensor or Sampler
+            Test data (prior)
+        noise_data : torch.Tensor or Sampler
+            Noise data for the flow matching objective         
+        noise_scaling : float
+            Noise scaling for the flow matching objective            
+        batchsize : int
+            Batchsize
+        n_print : int
+            Print interval
+        schedulers : iterable
+            A list of pairs (int, scheduler), where the integer specifies the number of iterations between
+            steps of the scheduler. Scheduler steps are invoked before the optimization step.
+        progress_bar : callable
+            To show a progress bar, pass `progress_bar = tqdm.auto.tqdm`
+
+        Returns
+        -------
+        """
+        if isinstance(prior_data, torch.Tensor):
+            prior_data = DataSetSampler(prior_data)
+        if isinstance(target_data, torch.Tensor):
+            target_data = DataSetSampler(target_data)
+        if isinstance(test_prior_data, torch.Tensor):
+            test_prior_data = DataSetSampler(test_prior_data)
+        if isinstance(test_target_data, torch.Tensor):
+            test_target_data = DataSetSampler(test_target_data)
+        if isinstance(noise_data, torch.Tensor):
+            noise_data = DataSetSampler(noise_data)     
+            
+        for iter in progress_bar(range(n_iter)):
+            # invoke schedulers
+            for interval, scheduler in schedulers:
+                if iter % interval == 0:
+                    scheduler.step()
+            self.optim.zero_grad()
+            reports = []
+            # FM loss
+            loss = self.flow_matching_loss(batchsize, prior_data, target_data, noise_data, noise_scaling)
+            loss.backward()
+            reports.append(loss)            
+                
+            # compute FM loss over test data 
+            if self.test_loss:
+                with torch.no_grad():
+                    test_loss = self.flow_matching_loss(batchsize, test_prior_data, test_target_data, noise_data, noise_scaling)
+                reports.append(test_loss)
+
+            self.reporter.report(*reports)
+            if n_print > 0:
+                if iter % n_print == 0:
+                    self.reporter.print(*reports)
+            
+            if any(torch.any(torch.isnan(p.grad)) for p in self.bg.parameters() if p.grad is not None):
+                print("found nan in grad; skipping optimization step")
+            else:
+                self.optim.step()
+
+
+    def losses(self, n_smooth=1):
+        return self.reporter.losses(n_smooth=n_smooth)
+
